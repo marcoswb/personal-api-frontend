8fc071dc0443dfe05b931206095413df85cae0d1
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-05-02 20:52:33 -0300

    update dependences

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Root from './components/Root';
 import Experience from './components/Experience';
