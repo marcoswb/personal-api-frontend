4526c9f355e946c2aa130570d88f480b301a23f0
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-05-03 20:59:12 -0300

    finished formation route

--- src/components/Formation/index.js
+++ src/components/Formation/index.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
+
 import './style.css';
 
-class Formation extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Formation</h2>
-        </div>
-    );
-  }
-}
+export default function Formation(){
 
-export default Formation;
+  const [formations, setFormations] = useState([])
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/formation`)
+      response = await response.json()
+
+      setFormations(response)
+    }
+
+    fetchMyAPI()
+  }, [])
+      
+  return (
+      <div>
+        <h3>formations:</h3>
+        {formations.map((x, i) => 
+          <p key={i}>{formations[i]['formation']}: {formations[i]['institution']}: {formations[i]['period']}</p>
+        )}
+      </div>
+  )
+}
