dd020d476002d633f44b137ce87929f242066640
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-05-03 20:56:47 -0300

    finished get information experience route

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
+
 import './style.css';
 
-class Experience extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Experience</h2>
-        </div>
-    );
-  }
-}
+export default function Experience(){
 
-export default Experience;
+  const [experiences, setExperiences] = useState([])
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/experience`)
+      response = await response.json()
+
+      setExperiences(response)
+    }
+
+    fetchMyAPI()
+  }, [])
+      
+  return (
+      <div>
+        <h3>experiences:</h3>
+        {experiences.map((x, i) => 
+          <p key={i}>{experiences[i]['company']}: {experiences[i]['ocuppation']}: {experiences[i]['period']}</p>
+        )}
+      </div>
+  )
+}
