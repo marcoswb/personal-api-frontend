7c3d5e111218a6f5bf712ac7cd2904a68ad07ed1
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-05-04 19:56:26 -0300

    finished route projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
+
 import './style.css';
 
-class Projects extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Projects</h2>
-        </div>
-    );
-  }
-}
+export default function Projects(){
 
-export default Projects;
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/projects`)
+      response = await response.json()
+
+      setProjects(response)
+    }
+
+    fetchMyAPI()
+  }, [])
+      
+  return (
+      <div>
+        <h3>projects:</h3>
+        {projects.map((x, i) =>
+          <>
+            <p key={i}>{projects[i]['name']}: {projects[i]['description']}: {projects[i]['link']}</p>
+            <h4>linguagens</h4>
+            {projects[i]['languages'].map((r, y) =>
+              <p key={y}>{projects[i]['languages'][y]}</p>
+            )}
+          </>
+        )}
+      </div>
+  )
+}
