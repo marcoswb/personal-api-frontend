5d95c6671b2d65c2230c6827e4c5bc9fff235959
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-05-04 20:05:04 -0300

    finished blog route

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
+
 import './style.css';
 
-class Blog extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Blog</h2>
-        </div>
-    );
-  }
-}
+export default function Blog(){
 
-export default Blog;
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/blog`)
+      response = await response.json()
+
+      setPosts(response)
+    }
+
+    fetchMyAPI()
+  }, [])
+      
+  return (
+      <div>
+        <h3>posts:</h3>
+        {posts.map((x, i) =>
+          <>
+            <p key={i}>{posts[i]['name']}: {posts[i]['description']}: {posts[i]['link']}</p>
+            <h4>categorias</h4>
+            {posts[i]['categories'].map((r, y) =>
+              <p key={y}>{posts[i]['categories'][y]}</p>
+            )}
+          </>
+        )}
+      </div>
+  )
+}
