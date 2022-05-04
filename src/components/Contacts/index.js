666822f2c7a2e72c0522cd505768a5ec8e9788dc
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-05-04 20:08:40 -0300

    finished contacts route

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
+
 import './style.css';
 
-class Contacts extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Contatos</h2>
-        </div>
-    );
-  }
-}
+export default function Contacts(){
 
-export default Contacts;
+  const [linkedin, setLinkedin] = useState('')
+  const [github, setGithub] = useState('')
+  const [email, setEmail] = useState('')
+  const [number_phone, setNumberPhone] = useState('')
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/contacts`)
+      response = await response.json()
+
+      setLinkedin(response['linkedin_link'])
+      setGithub(response['github_link'])
+      setEmail(response['email'])
+      setNumberPhone(response['number_phone'])
+    }
+
+    fetchMyAPI()
+  }, [])
+      
+  return (
+      <div>
+        <h3>linkedin:</h3>
+        <p>{linkedin}</p>
+        <h3>github:</h3>
+        <p>{github}</p>
+        <h3>email:</h3>
+        <p>{email}</p>
+        <h3>number_phone:</h3>
+        <p>{number_phone}</p>
+      </div>
+  )
+}
