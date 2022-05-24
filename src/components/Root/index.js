ac12393592b81d0098cf89a428c64d0340cc5460
Marcos Warmling Berti <marcos_wb@outlook.com>
2022-05-24 20:38:44 -0300

    finish footer

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -11,9 +11,6 @@ export default function Root(){
   const [whatsapp_link, setWhatsappLink] = useState('')
   const [about, setAbout] = useState('')
   const [skills, setSkills] = useState([])
-  const [linkedin, setLinkedin] = useState('')
-  const [github, setGithub] = useState('')
-  const [email, setEmail] = useState('')
 
   useEffect(() => {
     async function fetchMyAPI() {
@@ -25,9 +22,6 @@ export default function Root(){
       setWhatsappLink(`https://api.whatsapp.com/send/?phone=${response['number_phone']}&text=Olá ${response['name']}&app_absent=0`)
       setAbout(response['about'])
       setSkills(response['skills'])
-      setLinkedin(response['linkedin_link'])
-      setGithub(response['github_link'])
-      setEmail(response['email'])
     }
 
     fetchMyAPI()
@@ -51,14 +45,6 @@ export default function Root(){
               {skills.map((x, i) => 
                 <p key={i}>{skills[i]['name']}: {skills[i]['link_icon']}</p>
               )}
-            </div>
-            <div>
-              <h3>linkedin:</h3>
-              <p>{linkedin}</p>
-              <h3>github:</h3>
-              <p>{github}</p>
-              <h3>email:</h3>
-              <p>{email}</p>
             </div>
           </div>
         </div>
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,14 +4,20 @@ import './style.css';
 
 export default function Footer(){
 
-  const [full_name, setFullName] = useState('')
+  const [linkedin, setLinkedin] = useState('')
+  const [github, setGithub] = useState('')
+  const [email_link, setEmailLink] = useState('')
+  const [whatsapp_link, setWhatsappLink] = useState('')
 
   useEffect(() => {
     async function fetchMyAPI() {
       let response = await fetch(process.env.REACT_APP_ENDPOINT_API)
       response = await response.json()
 
-      setFullName(response['full_name'])
+      setLinkedin(response['linkedin_link'])
+      setGithub(response['github_link'])
+      setEmailLink(`mailto:${response['email']}?subject=Contato%20via%20site`)
+      setWhatsappLink(`https://api.whatsapp.com/send/?phone=${response['number_phone']}&text=Olá ${response['name']}&app_absent=0`)
     }
 
     fetchMyAPI()
@@ -22,10 +28,10 @@ export default function Footer(){
     <footer>
       <div class="footer-content">
         <ul class="socials">
-          <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
-          <li><a href="#"><i class="fa fa-github"></i></a></li>
-          <li><a href="#"><i class="fa fa-envelope"></i></a></li>
-          <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
+          <li><a href={linkedin} target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
+          <li><a href={github} target="_blank"><i class="fa fa-github"></i></a></li>
+          <li><a href={email_link} target="_blank"><i class="fa fa-envelope"></i></a></li>
+          <li><a href={whatsapp_link} target="_blank"><i class="fa fa-whatsapp"></i></a></li>
         </ul>
       </div>
     </footer>
