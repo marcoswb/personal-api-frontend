894f771d6fce207ec059e63c5b33472d5cddfd1f
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-06-14 09:10:52 -0300

    adjustment style contact page

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -29,16 +29,16 @@ export default function Contacts(){
       <div>
         <Navbar/>
         <div class='main-content'>
-          <a href={linkedin} target="_blank" rel="noreferrer">
+          <a href={linkedin} target="_blank" rel="noreferrer" class="contact">
             <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="logo LinkedIn"/>
           </a>
-          <a href={github} target="_blank" rel="noreferrer">
+          <a href={github} target="_blank" rel="noreferrer" class="contact">
             <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="logo GitHub"/>
           </a>
-          <a href={email} target="_blank" rel="noreferrer">
+          <a href={email} target="_blank" rel="noreferrer" class="contact">
             <img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt="logo Email"/>
           </a>
-          <a href={number_phone} target="_blank" rel="noreferrer">
+          <a href={number_phone} target="_blank" rel="noreferrer" class="contact">
             <img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" alt="logo WhatsApp"/>
           </a>
         </div>
