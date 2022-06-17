d53047baccb7dce03c0c12d68397f64dda913c2c
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-06-17 10:48:26 -0300

    add submenu project page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,32 +7,75 @@ import './style.css';
 export default function Projects(){
 
   const [projects, setProjects] = useState([])
+  const [languages, setLanguages] = useState([])
 
-  useEffect(() => {
-    async function fetchMyAPI() {
-      let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/projects`)
-      response = await response.json()
+  async function loadLanguages(){
+    let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/projects`)
+    response = await response.json()
 
-      setProjects(response)
+    let aux = []
+    for(var index in response){
+      for(var index_language in response[index]['languages']){
+        let language = String(response[index]['languages'][index_language]).toUpperCase()
+        if(!aux.includes(language) && language !== ''){
+          aux.push(language)
+        }
+      }
     }
+    
+    setLanguages(aux)
+  }
 
-    fetchMyAPI()
+  async function loadAllProjects(){
+    let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/projects`)
+    response = await response.json()
+
+    setProjects(response)
+  }
+  
+  useEffect(() => {
+    loadAllProjects()
+    loadLanguages()
   }, [])
+
+  async function filterLanguages(event){
+    let language_filter = event.target.textContent.toUpperCase()
+    let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/projects`)
+    response = await response.json()
+
+    let aux = []
+    for(var index in response){
+      for(var index_language in response[index]['languages']){
+        let language = String(response[index]['languages'][index_language]).toUpperCase()
+        if(language_filter === language){
+          aux.push(response[index])
+        }
+      }
+    }
+    
+    setProjects(aux)
+  }
       
   return (
       <div>
         <Navbar/>
-        <div class='main-content'>
-        {projects.map((x, i) =>
-            <a href={projects[i]["link"]} target="_blank" rel="noreferrer" class='project'>
-              <h2>{projects[i]['name']}</h2>
-              <h4>{projects[i]['description']}</h4>
-              {/* <h4>categorias</h4>
-              {projects[i]['categories'].map((r, y) =>
-                <p key={y}>{projects[i]['categories'][y]}</p>
-              )} */}
-            </a>
-          )}
+        <div id="content-list" class="main-content">
+          <div id="projects">
+            {projects.map((x, i) =>
+              <a href={projects[i]["link"]} target="_blank" rel="noreferrer" class='project'>
+                <h2>{projects[i]['name']}</h2>
+                <h4>{projects[i]['description']}</h4>
+              </a>
+            )}
+          </div>
+          <div id="project-languages">
+            <h3 onClick={loadAllProjects}>Linguages e Ferramentas</h3>
+            <ol>
+              {languages.map((x, i) =>
+                <li onClick={filterLanguages}>{languages[i]}</li>
+              )}
+            </ol>
+          </div>
         </div>
         <Footer/>
       </div>
