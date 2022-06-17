9a6c838058fd7e72e6b8a0005c3a97ccb2d09139
Marcos Warmling Berti <marcos_+wb@outlook.com>
2022-06-17 10:36:17 -0300

    add submenu

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -7,32 +7,75 @@ import './style.css';
 export default function Blog(){
 
   const [posts, setPosts] = useState([])
+  const [categories, setCategories] = useState([])
 
-  useEffect(() => {
-    async function fetchMyAPI() {
-      let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/blog`)
-      response = await response.json()
+  async function loadCategories(){
+    let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/blog`)
+    response = await response.json()
 
-      setPosts(response)
+    let aux = []
+    for(var index in response){
+      for(var index_category in response[index]['categories']){
+        let category = String(response[index]['categories'][index_category]).toUpperCase()
+        if(!aux.includes(category)){
+          aux.push(category)
+        }
+      }
     }
+    
+    setCategories(aux)
+  }
 
-    fetchMyAPI()
+  async function loadAllPosts(){
+    let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/blog`)
+    response = await response.json()
+
+    setPosts(response)
+  }
+  
+  useEffect(() => {
+    loadAllPosts()
+    loadCategories()
   }, [])
+
+  async function filterPosts(event){
+    let category_filter = event.target.textContent.toUpperCase()
+    let response = await fetch(`${process.env.REACT_APP_ENDPOINT_API}/blog`)
+    response = await response.json()
+
+    let aux = []
+    for(var index in response){
+      for(var index_category in response[index]['categories']){
+        let category = String(response[index]['categories'][index_category]).toUpperCase()
+        if(category_filter === category){
+          aux.push(response[index])
+        }
+      }
+    }
+    
+    setPosts(aux)
+  }
       
   return (
       <div>
         <Navbar/>
-        <div class='main-content'>
-          {posts.map((x, i) =>
-            <a href={posts[i]["link"]} target="_blank" rel="noreferrer" class='post'>
-              <h2>{posts[i]['name']}</h2>
-              <h4>{posts[i]['description']}</h4>
-              {/* <h4>categorias</h4>
-              {posts[i]['categories'].map((r, y) =>
-                <p key={y}>{posts[i]['categories'][y]}</p>
-              )} */}
-            </a>
-          )}
+        <div id="content-list" class="main-content">
+          <div id="blog-posts">
+            {posts.map((x, i) =>
+              <a href={posts[i]["link"]} target="_blank" rel="noreferrer" class='post'>
+                <h2>{posts[i]['name']}</h2>
+                <h4>{posts[i]['description']}</h4>
+              </a>
+            )}
+          </div>
+          <div id="blog-techs">
+            <h3 onClick={loadAllPosts}>Assuntos</h3>
+            <ol>
+              {categories.map((x, i) =>
+                <li onClick={filterPosts}>{categories[i]}</li>
+              )}
+            </ol>
+          </div>
         </div>
         <Footer/>
       </div>
